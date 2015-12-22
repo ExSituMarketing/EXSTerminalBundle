77f911e400d83f792d1945f4e8c259cb95e4a737
Olivier Maisonneuve <olivierm@ex-situ.com>
2015-12-22 11:30:27 -0500

    compiler pass for email manager transport

--- EXSTerminalBundle.php
+++ EXSTerminalBundle.php
@@ -2,8 +2,24 @@
 
 namespace EXS\TerminalBundle;
 
+use EXS\TerminalBundle\DependencyInjection\CompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class EXSTerminalBundle
+ *
+ * @package EXS\TerminalBundle
+ */
 class EXSTerminalBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new CompilerPass());
+    }
 }
